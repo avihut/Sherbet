795499ffe032307e420959d9517ba7b6ec1b959e
Avihu Turzion <avihu.turzion@gmail.com>
2020-03-24 01:15:24 +0200

    Removed keyboard response code for not working.

--- Chatbot/View/ChatViewController.swift
+++ Chatbot/View/ChatViewController.swift
@@ -14,24 +14,4 @@ final class ChatViewController: UIViewController {
     @IBOutlet private weak var inputArea: UIView!
     @IBOutlet private weak var chatTableView: UITableView!
     @IBOutlet private weak var inputAreaBottomConstraint: NSLayoutConstraint!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height - self.view.safeAreaInsets.bottom
-            }
-        }
-    }
-
-    @objc func keyboardWillHide(notification: NSNotification) {
-        if self.view.frame.origin.y != 0 {
-            self.view.frame.origin.y = 0
-        }
-    }
 }
