fde8623911ede0beffe52faf8b8a615adf7bbccd
Avihu Turzion <avihu.turzion@gmail.com>
2020-03-24 01:06:43 +0200

    Created wireframe view and added keyboard height response.

---
+++ Chatbot/View/ChatViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ChatViewController.swift
+//  Chatbot
+//
+//  Created by Avihu Turzion on 24/03/2020.
+//  Copyright © 2020 Avihu Turzion. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+final class ChatViewController: UIViewController {
+    @IBOutlet private weak var inputArea: UIView!
+    @IBOutlet private weak var chatTableView: UITableView!
+    @IBOutlet private weak var inputAreaBottomConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height - self.view.safeAreaInsets.bottom
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+}
